d007968ea819e69cec6608685fde892f13dd4796
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-03-29 08:52:28 -0300

    foi adicionado o Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Routes />
       <GlobalStyles/>
-    </div>
+    </>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            
+        </Switch>
+    </BrowserRouter> 
+);
+export default Routes;
+
+//<Route exact path="/" component={} />
